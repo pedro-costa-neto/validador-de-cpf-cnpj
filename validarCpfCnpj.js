c9f7d3d9fd243a6409785919e8aa505954a24a15
Pedro Costa <pcon1996@gmail.com>
2019-10-19 01:15:19 -0300

    Alterando script.

--- validarCpfCnpj.js
+++ validarCpfCnpj.js
@@ -57,23 +57,3 @@ function validarCpfCnpj(campo){
 		alert(mensagem);
 	}
 }
-
-
-
-
-function mCNPJ(cnpj){
-				cnpj=cnpj.replace(/\D/g,"")
-				cnpj=cnpj.replace(/^(\d{2})(\d)/,"$1.$2")
-				cnpj=cnpj.replace(/^(\d{2})\.(\d{3})(\d)/,"$1.$2.$3")
-				cnpj=cnpj.replace(/\.(\d{3})(\d)/,".$1/$2")
-				cnpj=cnpj.replace(/(\d{4})(\d)/,"$1-$2")
-				return cnpj
-			}
-			function mCPF(cpf){
-				cpf=cpf.replace(/\D/g,"")
-				cpf=cpf.replace(/(\d{3})(\d)/,"$1.$2")
-				cpf=cpf.replace(/(\d{3})(\d)/,"$1.$2")
-				cpf=cpf.replace(/(\d{3})(\d{1,2})$/,"$1-$2")
-				return cpf
-			}
-			functio
